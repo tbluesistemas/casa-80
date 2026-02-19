7ab3e0df7ab7276166fd9f5b32d19457a1f25649
Luisecorcia1 <luisescorcia51@gmail.com>
2026-02-19 13:59:58 -0500

    Correccion 1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,6 +8,16 @@ export const prisma = globalForPrisma.prisma || new PrismaClient({
             url: process.env.DATABASE_URL,
         },
     },
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
 })
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+    if (!globalForPrisma.prisma) {
+        console.log('⚡️ [Prisma] Creating new client instance')
+    }
+    globalForPrisma.prisma = prisma
+}
